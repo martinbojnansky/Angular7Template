2103765f06f6ab108a4ec8f1c8877ac372b3c939
martinbojnansky <martin.bojnansky@outlook.com>
2019-01-15 14:19:08 +0100

    Adds user detail tests.

--- angular7-template/src/app/features/users/components/user-detail/user-detail.component.spec.ts
+++ angular7-template/src/app/features/users/components/user-detail/user-detail.component.spec.ts
@@ -1,10 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailComponent } from './user-detail.component';
+import { User } from '../../models';
 
-describe('DetailComponent', () => {
+describe('UserDetailComponent', () => {
   let component: UserDetailComponent;
   let fixture: ComponentFixture<UserDetailComponent>;
+  let compiled: any;
+  const userMock = {
+    id: 1,
+    first_name: 'fn',
+    last_name: 'ln',
+    avatar: 'av'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,10 +23,44 @@ describe('DetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailComponent);
     component = fixture.componentInstance;
+    component.user = Object.assign({}, userMock);
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display content when user is set', () => {
+    const section = compiled.querySelector('section');
+
+    expect(section).toBeTruthy();
+  });
+
+  it('should not display when user is not set', () => {
+    component.user = null;
+    fixture.detectChanges();
+    const section = compiled.querySelector('section');
+
+    expect(section).toBeNull();
+  });
+
+  it('should display first name', () => {
+    const section = compiled.querySelector('p').textContent;
+
+    expect(section).toBe(userMock.first_name);
+  });
+
+  it('should emit closed on button click', () => {
+    const button = compiled.querySelector('#close-btn');
+    let emittedUser: User;
+
+    component.closed.subscribe(u => {
+      emittedUser = u;
+    });
+    button.click();
+
+    expect(emittedUser.id).toBe(userMock.id);
   });
 });
