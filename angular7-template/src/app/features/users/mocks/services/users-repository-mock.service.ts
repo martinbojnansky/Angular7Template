035a27fd95485d9ec645c688fc5534ae86afd4fc
martinbojnansky <martin.bojnansky@outlook.com>
2019-01-16 14:49:35 +0100

    Removes unnecesary import.

--- angular7-template/src/app/features/users/mocks/services/users-repository-mock.service.ts
+++ angular7-template/src/app/features/users/mocks/services/users-repository-mock.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UsersRepositoryService } from '../../services';
-import { User } from '../../models';
 
 @Injectable()
 export class UsersRepositoryServiceMock extends UsersRepositoryService {
