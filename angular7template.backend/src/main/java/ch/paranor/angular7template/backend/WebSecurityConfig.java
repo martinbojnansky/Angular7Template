25083f5808c7b6a35ba4eb1afd52826692a97ccf
Maxim Gordienko <max.gordienko@paranor.ch>
2019-02-06 13:31:14 +0100

    Add the server side security
    Login is done by sending a POST request to /login with 2 form fields: username
    and password.
    Logout is done by sending an empty POST request to /logout.

---
+++ angular7template.backend/src/main/java/ch/paranor/angular7template/backend/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package ch.paranor.angular7template.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+  }
+}
