35fb0b10ecc5433d05ab8188496f5c9edd809fc5
Maxim Gordienko <maxim@dgap.mipt.ru>
2019-02-10 23:14:07 +0100

    JWT

---
+++ angular7template.backend/src/main/java/ch/paranor/angular7template/backend/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package ch.paranor.angular7template.backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		return super.userDetailsService();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().apply(new JwtConfigurer(jwtTokenProvider));
+	}
+}
